d0147854be713ea215a63df24ad01ba9177a2ece
gnpavankumar <gnpavan2004@gmail.com>
2025-08-08 23:06:24 +0530

    updated the accounts app

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -2,6 +2,24 @@ from  rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only=True, required=True)
     class Meta:
         model=User
-        fields=['']
+        fields=['id','username','password','name','phone','role','email']
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user=User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def update(self, instance, validated_data):
+        password=validated_data.pop('password',None)
+        for attr, value in validated_data.items:
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
